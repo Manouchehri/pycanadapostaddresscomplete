d61a75cdb55d66e52c8e53aa9f710337649aaf7e
David Manouchehri <david@davidmanouchehri.com>
2015-02-22 02:22:01 -0330

    Added URLs and GET for retrieving.

--- get_address.py
+++ get_address.py
@@ -12,7 +12,9 @@ headers = {'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;
            'Accept-Language': 'en-US,en;q=0.5',
            'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:35.0) Gecko/20100101 Firefox/35.0'}
 
-base_url = 'https://ws1.postescanada-canadapost.ca/AddressComplete/Interactive/Find/v2.10/json3ex.ws?'
+base_site = 'https://ws1.postescanada-canadapost.ca/AddressComplete/Interactive/'
+base_find_url = base_site + 'Find/v2.10/json3ex.ws?'
+base_search_url = base_site + 'RetrieveFormatted/v2.10/json3ex.ws?'
 
 key = 'HT97-HU28-TJ75-KE42'  # This is the public access key, replace with your own.
 
@@ -29,12 +31,15 @@ search_params = {'Key': key,
                  '$block': 'true',
                  '$cache': 'true'}
 
+retrieve_params = {'Key': key,
+                   'Id': '',
+                   'Source': '',
+                   '$cache': 'true'}
+
 if __name__ == "__main__":
     import sys
     search_params['SearchTerm'] = ' '.join(sys.argv[1:]).strip()
     print(search_params['SearchTerm'])
-
-url = base_url + urllib.parse.urlencode(search_params)
 
 
 def grab_url_data(url):
@@ -49,8 +54,8 @@ def grab_url_data(url):
     return page_data
 
 
-data = grab_url_data(url).decode('utf-8')
+data = grab_url_data(base_find_url + urllib.parse.urlencode(search_params)).decode('utf-8')
 
 parsed = json.loads(data)
-print(parsed)
+
 print(json.dumps(parsed, indent=4))
